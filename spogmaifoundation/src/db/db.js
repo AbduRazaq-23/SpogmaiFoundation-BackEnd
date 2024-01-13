0575e7a9dda4c8a4993980713acf3983caf011b8
Abdurazaq333 <irazaqr986@gmail.com>
2024-01-13 19:03:19 +0500

    db connected message

--- spogmaifoundation/src/db/db.js
+++ spogmaifoundation/src/db/db.js
@@ -4,7 +4,7 @@ import { DB_NAME } from "../constant.js";
 const connectDB = async () => {
   try {
     const connect = await mongoose.connect(`${process.env.DB_URL}/${DB_NAME}`);
-    console.log(connect);
+    console.log("DB connected");
   } catch (error) {
     console.log("dataBase error");
   }
