417d0f544ccfedd2c749da05eccb0df6c044f452
Abdurazaq333 <irazaqr986@gmail.com>
2024-01-17 23:25:03 +0500

    added three utils fils

---
+++ spogmaifoundation/src/utils/ApiError.js
@@ -0,0 +1,31 @@
+class ApiError extends Error {
+  constructor(
+    statusCode,
+    message = "something went wrong",
+    errors = [],
+    stack = ""
+  ) {
+    super(message);
+
+    this.statusCode = statusCode;
+    this.data = null; // Optional data payload that can be attached to the error
+    this.message = message;
+    this.success = false; // Indicates whether the operation that led to this error was successful or not
+    this.errors = errors; // Array of additional error details
+
+    // If a stack trace is provided, use it; otherwise, capture the stack trace
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+export { ApiError };
+
+console.log(ApiError.message);
+console.log(ApiError.statusCode);
+console.log(ApiError.stack);
+console.log(ApiError.errors);
+console.log(ApiError.success);
